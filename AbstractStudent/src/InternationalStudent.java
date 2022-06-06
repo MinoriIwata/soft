0b373bc2ad84710e788f85588328b6e9bd4006fb
rinne <rinne@DESKTOP-EC4J4EG>
2022-06-07 00:32:17 +0900

    re

--- AbstractStudent/src/InternationalStudent.java
+++ AbstractStudent/src/InternationalStudent.java
@@ -1,4 +1,17 @@
 
 public class InternationalStudent {
+	private String country;
+	private boolean kokuhi;
+	public InternationalStudent(){
+		super();
+	}
+	public InternationalStudent(String id, String name, int credit,String country,boolean kokuhi) {
+		super(id, name, credit,country,kokuhi);
+	}
 
+	//抽象メソッドをオーバーライド．しないとエラーになる．
+	@Override
+	public void explain() {
+		System.out.println("私" + getName() + "は，正規の学生です．");
+	}
 }
--- AbstractStudent/src/RegularStudent.java
+++ AbstractStudent/src/RegularStudent.java
@@ -1,4 +1,17 @@
+public class RegularStudent extends Student {
+	/* -------     コンストラクタ-------------------*/
+	public RegularStudent(){
+		super();
+	}
+	public RegularStudent(String id, String name, int credit) {
+		super(id, name, credit);
+	}
 
-public class RegularStudent {
+	//抽象メソッドをオーバーライド．しないとエラーになる．
+	@Override
+	public void explain() {
+		System.out.println("私" + getName() + "は，正規の学生です．");
+	}
 
+	
 }
