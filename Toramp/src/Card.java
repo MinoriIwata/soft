f8b0723326803a812630589c5df7ca71de8b7724
rinne <2055045t@stu.kobe-u.ac.jp>
2022-07-11 16:44:02 +0900

    re

--- Toramp/src/Card.java
+++ Toramp/src/Card.java
@@ -6,7 +6,52 @@ public class Card {
 	 public Card (int suit, int number) {
 		 
 	 }
+
+	public int getSuit() {
+		return suit;
+	}
+
+	public void setSuit(int suit) {
+		this.suit = suit;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 	 
+	public int tolndex() {
+		return number;
+		
+	}
+	
+	public void show() {
+		
+	}
+	
+	public static int getIndex(int suit,int number)
+	{
+		return number;
+		
+	}
+	
+	
+	public static String getString(int suit,int number)
+	{
+		return null;
+		
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Card [suit=" + suit + ", number=" + number + "]";
+	}
+	
+ 
 	
 	 
 	 
