1341478101c915e9820b16db0959db812d1be718
rinne <2055045t@stu.kobe-u.ac.jp>
2022-07-11 17:44:00 +0900

    re

--- Toramp/src/Card.java
+++ Toramp/src/Card.java
@@ -1,59 +1,158 @@
 
 public class Card {
+	/** スート */
 	private int suit;
+	/** 番号 */
 	private int number;
-	 
-	 public Card (int suit, int number) {
-		 
-	 }
 
+	/**
+	 * デフォルトコンストラクタ。空のカードインスタンスを生成する。
+	 */
+	public Card() {
+
+	}
+
+	/**
+	 * スート、番号を指定してカードインスタンスを生成する.
+	 * 
+	 * @param suit   スート
+	 * @param number 番号
+	 */
+	public Card(int suit, int number) {
+		this.suit = suit;
+		this.number = number;
+	}
+
+	/**スートを取得する */
 	public int getSuit() {
 		return suit;
 	}
-
-	public void setSuit(int suit) {
-		this.suit = suit;
-	}
-
+	/**数字を取得する */
 	public int getNumber() {
 		return number;
 	}
 
-	public void setNumber(int number) {
-		this.number = number;
-	}
-	 
+	/**
+	 * カード情報を整数表現に変換する.
+	 * スペード→ダイヤ→ハート→クラブの1~13を、この順番で0から51までの通し番号をつける.
+	 * ジョーカーは-1である.
+	 */
 	public int tolndex() {
-		return number;
-		
+		int t = (this.suit * 13) + this.number - 1;
+		if (t < 0) {
+			return -1;
+		} else {
+			return t;
+		}
 	}
 	
+	/**カード情報を画面に出力する.*/
 	public void show() {
-		
-	}
-	
-	public static int getIndex(int suit,int number)
-	{
-		return number;
-		
-	}
-	
-	
-	public static String getString(int suit,int number)
-	{
-		return null;
-		
-	}
-	
 
+		String carddata = this.toString();
+
+		System.out.println(carddata);
+	}
+
+	/**
+	 * 与えられた絵柄と数字を基に、対応するカードの整数表現を返す
+	 * 
+	 * @param suit   スート
+	 * @param number 番号
+	 */
+	public static int getIndex(int suit, int number) {
+		int t = (suit * 13) + number - 1;
+		if (t < 0) {
+			return -1;
+		} else {
+			return t;
+		}
+
+	}
+	
+	/**
+	 * 与えられた絵柄と数字を基に、対応するカードの文字列表現を返す
+	 * 
+	 * @param suit   スート
+	 * @param number 番号
+	 */
+	public static String getString(int suit, int number) {
+		String su, num;
+
+		switch (suit) {
+		case 0:
+			su = "スペード";
+		case 1:
+			su = "ダイヤ";
+
+		case 2:
+			su = "ハート";
+
+		case 3:
+			su = "クラブ";
+
+		default:
+			su = "スート判別不可";
+		}
+
+		switch (number) {
+		case 1:
+			num = "A";
+		case 11:
+			num = "J";
+		case 12:
+			num = "Q";
+		case 13:
+			num = "K";
+		default:
+			num = Integer.toString(number);
+		}
+
+		if (suit == 0 && number == -1) {
+			return "ジョーカー";
+		}
+		return su + num;
+
+	}
+
+	/**カード情報を文字列表現に変換する.*/
 	@Override
 	public String toString() {
-		return "Card [suit=" + suit + ", number=" + number + "]";
+		String su, num;
+
+		switch (suit) {
+		case 0:
+			su = "スペード";
+		case 1:
+			su = "ダイヤ";
+
+		case 2:
+			su = "ハート";
+
+		case 3:
+			su = "クラブ";
+
+		default:
+			su = "スート判別不可";
+		}
+
+		switch (number) {
+		case 1:
+			num = "A";
+		case 11:
+			num = "J";
+		case 12:
+			num = "Q";
+		case 13:
+			num = "K";
+		default:
+			num = Integer.toString(number);
+		}
+
+		if (suit == 0 && number == -1) {
+			return "ジョーカー";
+		}
+		return su + num;
 	}
-	
- 
-	
-	 
-	 
 
 }
--- Toramp/src/CardDeck.java
+++ Toramp/src/CardDeck.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class CardDeck {
 	
@@ -9,11 +11,91 @@ public class CardDeck {
 	
 	public void createFullDeck() {
 		
+		System.out.println("フルデッキを生成します.");
+		Card newcard;
+		for(int i=0;i<4;++i)
+		{
+			for(int j=0;j<13;++j)
+			{
+			 newcard=new Card(i,j);
+			 cards.add(newcard);
+			}
+		}
 	}
 	
 	public void clear() {
-		
+		cards.clear();
+	}
+	
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+	
+	public void addCard(Card card)
+	{
+		cards.add(card);
 	}
 	
 
+	public void addCard(int i,Card card)
+	{
+		cards.add(i-1,card);
+	}
+	
+	public Card takeCard() {
+		return null;
+		
+	}
+	
+	public Card takeCard(int i)
+	{
+		return null;
+		
+	}
+	
+	public Card seeCard(int i)
+	{
+		return null;
+		
+	}
+	
+	public int searchCard(int suit,int number)
+	{
+		return number;
+		
+	}
+	
+	/**現在のデッキが空かどうか判定する.*/
+	public boolean isEmpty()
+	{
+		if(cards.isEmpty()==true)
+		{
+		return true;
+		}
+		else
+		{
+		return false;
+		}
+		
+	}
+	
+	/**現在のデッキのカード枚数を返す.*/
+	public int size()
+	{
+		int size=cards.size();
+		return size;
+		
+	}
+	
+	public void showAllCards()
+	{
+		
+	}
+	
+	public List<Card> getAllCards()
+	{
+		return cards;
+		
+	}
+	
 }
