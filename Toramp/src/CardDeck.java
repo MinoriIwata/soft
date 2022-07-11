eba897ed94bce5267ca00e65c8878c4ff205a698
rinne <2055045t@stu.kobe-u.ac.jp>
2022-07-11 16:47:46 +0900

    re

---
+++ Toramp/src/CardDeck.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class CardDeck {
+	
+	private  ArrayList<Card> cards;
+	
+	public CardDeck() {
+	}
+	
+	public void createFullDeck() {
+		
+	}
+	
+	public void clear() {
+		
+	}
+	
+
+}
