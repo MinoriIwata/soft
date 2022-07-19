c1cc22889040ad546c9ceab3cca30508fdfd58f2
rinne <2055045t@stu.kobe-u.ac.jp>
2022-07-19 11:44:47 +0900

    re

--- Toramp/src/se/t2055405/card/game/KeyBoard.java
+++ Toramp/src/se/t2055405/card/game/KeyBoard.java
@@ -1,5 +1,32 @@
 package se.t2055405.card.game;
 
-public class KeyBoard {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class KeyBoard {
+	public static Integer inputNumber()
+	{
+		String line;
+        line=inputString();
+        int num=Integer.parseInt(line);
+		return num;
+	}
+	
+	
+	public static String inputString()
+	{
+		String line;
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			line = br.readLine();
+		} catch (IOException e) {
+			System.err.println("エラー：入出力例外です．もう一度入力．");
+			line = inputString();
+		}
+
+		return line;
+
+	}
 }
